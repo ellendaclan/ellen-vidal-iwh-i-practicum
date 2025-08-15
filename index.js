2b3bb4b4737e806bb60bd03b157bc1fe45124b18
Ellen Daclan <109201824+ellendaclan@users.noreply.github.com>
2025-08-15 11:53:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,61 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// TODO: ROUTE 1 - Homepage route
+app.get('/', async (req, res) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${process.env.HUBSPOT_CUSTOM_OBJECT_TYPE}`;
+    const headers = {
+        Authorization: `Bearer ${process.env.HUBSPOT_PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
 
-// * Code for Route 1 goes here
+    try {
+        const resp = await axios.get(url, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Custom Objects | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+        res.send('Error loading homepage');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+// TODO: ROUTE 2 - Show form to create/update
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+TODO: ROUTE 3 - Handle form submit
+app.post('/update-cobj', async (req, res) => {
+    const objectId = req.body.objectId; // Optional: update existing
+    const properties = {
+        "example_property": req.body.example_property
+    };
+
+    const url = objectId
+        ? `https://api.hubapi.com/crm/v3/objects/${process.env.HUBSPOT_CUSTOM_OBJECT_TYPE}/${objectId}`
+        : `https://api.hubapi.com/crm/v3/objects/${process.env.HUBSPOT_CUSTOM_OBJECT_TYPE}`;
+
+    const headers = {
+        Authorization: `Bearer ${process.env.HUBSPOT_PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        if (objectId) {
+            await axios.patch(url, { properties }, { headers });
+        } else {
+            await axios.post(url, { properties }, { headers });
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.send('Error updating custom object');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
